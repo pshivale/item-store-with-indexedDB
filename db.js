cb871fa514e174c17ce1da9b404746a8fdd189c9
pshivale <pshivale@gmail.com>
2013-09-26 09:30:51 +0530

    add reinitialize db

--- db.js
+++ db.js
@@ -2,13 +2,16 @@ var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedD
 var IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction;
 var db;
 (function () {     
-    var peopleData = [
-        { name: "John Dow", email: "john@company.com" },
-        { name: "Don Dow", email: "don@company.com" }
+    var itemData = [
+        { name: "Aminophylline : 225 mg", type: "Tablet", price: "5.09" },
+        { name: "Vit E : 100 mg", type: "Capsule", price: "5.34" },
+        { name: "Analgin : 500 mg", type: "Injection", price: "4.54" },
+        { name: "Analgin : 500 mg", type: "Tablet", price: "8.27" },
+        { name: "Aspirin : 50 mg", type: "Tablet", price: "1.1" }
     ];
 
     function initDb() {
-        var request = indexedDB.open("PeopleDB", 1);  
+        var request = indexedDB.open("ItemDB", 1);  
         request.onsuccess = function (evt) {
             db = request.result;                                                            
         };
@@ -19,13 +22,12 @@ var db;
 
         request.onupgradeneeded = function (evt) {                   
             var objectStore = evt.currentTarget.result.createObjectStore(
-                     "people", { keyPath: "id", autoIncrement: true });
+                     "item", { keyPath: "id", autoIncrement: true });
 
             objectStore.createIndex("name", "name", { unique: false });
-            objectStore.createIndex("email", "email", { unique: true });
 
-            for (i in peopleData) {
-                objectStore.add(peopleData[i]);
+            for (i in itemData) {
+                objectStore.add(itemData[i]);
             }
         };
     }
@@ -34,26 +36,41 @@ var db;
         initDb();                
         var btnAdd = document.getElementById("btnAdd");
         var btnDelete = document.getElementById("btnDelete");
-        var btnPrint = document.getElementById("btnPrint");                
+        var btnPrint = document.getElementById("btnPrint");
+        var reinitialize_db = document.getElementById("reinitialize_db");
 
         btnAdd.addEventListener("click", function () {
             var name = $("#name").val();
             var price = $("#price").val();
 
-            // var transaction = db.transaction("people", IDBTransaction.READ_WRITE);
-            var transaction = db.transaction(["people"], "readwrite")
-            var objectStore = transaction.objectStore("people");                    
+            // var transaction = db.transaction("item", IDBTransaction.READ_WRITE);
+            var transaction = db.transaction(["item"], "readwrite")
+            var objectStore = transaction.objectStore("item");                    
             var request = objectStore.add({ name: name, price: price });
             request.onsuccess = function (evt) {
                 // do something after the add succeeded
             };
         }, false);
 
+        reinitialize_db.addEventListener("click", function () {
+
+            var transaction = db.transaction(["item"], "readwrite");//db.transaction("item", IDBTransaction.READ_WRITE);
+            var objectStore = transaction.objectStore("item");
+            objectStore.clear().onsuccess = function(event) {
+                console.log("Database cleared");
+            };
+            transaction = db.transaction(["item"], "readwrite");//db.transaction("item", IDBTransaction.READ_WRITE);
+            objectStore = transaction.objectStore("item");
+            for (i in itemData) {
+                objectStore.add(itemData[i]);
+            }
+        }, false);
+
         // btnDelete.addEventListener("click", function () {
         //     var id = document.getElementById("txtID").value;
 
-        //     var transaction = db.transaction(["people"], "readwrite");//db.transaction("people", IDBTransaction.READ_WRITE);
-        //     var objectStore = transaction.objectStore("people");
+        //     var transaction = db.transaction(["item"], "readwrite");//db.transaction("item", IDBTransaction.READ_WRITE);
+        //     var objectStore = transaction.objectStore("item");
         //     var request = objectStore.delete(id);
         //     request.onsuccess = function(evt) {  
         //         // It's gone!  
@@ -62,16 +79,17 @@ var db;
 
         btnPrint.addEventListener("click", function () {
             var products_tbody = $("#products_tbody");
-
-            var transaction = db.transaction(["people"], "readwrite")//= db.transaction("people", IDBTransaction.READ_WRITE);
-            var objectStore = transaction.objectStore("people");
+            products_tbody.html('');
+            var transaction = db.transaction(["item"], "readwrite")//= db.transaction("item", IDBTransaction.READ_WRITE);
+            var objectStore = transaction.objectStore("item");
 
             var request = objectStore.openCursor();
             request.onsuccess = function(evt) {  
+
                 var cursor = evt.target.result;  
                 if (cursor) {  
                     products_tbody.append("<tr>"
-                        + getCell(cursor.key)
+                        // + getCell(cursor.key)
                         + getCell(cursor.value.name)
                         + getCell(cursor.value.price)
                         + "</tr>");
